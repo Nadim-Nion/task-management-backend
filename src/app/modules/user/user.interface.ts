667fbc1d319084bcd505739985c780d76ef3bdb1
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2025-07-30 23:20:24 +0600

    static method are added for find user is exists and matching the password. Pre middleware is used to hashed the password

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,5 +1,16 @@
+import { Model } from "mongoose";
+
+/* eslint-disable no-unused-vars */
 export type TUser = {
   name: string;
   email: string;
   password: string;
 };
+
+export interface UserModelType extends Model<TUser> {
+  isUserExistsByCustomId(id: string): Promise<TUser>;
+  isPasswordMatched(
+    plainTextPassword: string,
+    hashedPassword: string,
+  ): Promise<boolean>;
+}
--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,7 +1,9 @@
 import { model, Schema } from 'mongoose';
-import { TUser } from './user.interface';
+import { TUser, UserModelType } from './user.interface';
+import bcrypt from 'bcrypt';
+import config from '../../config';
 
-const userSchema = new Schema<TUser>(
+const userSchema = new Schema<TUser,UserModelType>(
   {
     name: {
       type: String,
@@ -24,4 +26,35 @@ const userSchema = new Schema<TUser>(
   },
 );
 
-export const User = model<TUser>('User', userSchema);
+// Pre save middleware/hook: will work on create() or save()
+userSchema.pre('save', async function (next) {
+  // console.log(this, 'pre hook: we will save the data');
+  // const user = this;
+  this.password = await bcrypt.hash(
+    this.password,
+    Number(config.bcrypt_salt_round),
+  );
+  next();
+});
+
+// Post save middleware/hook
+userSchema.post('save', function (doc, next) {
+  doc.password = '';
+  next();
+});
+
+// Check the user is existed or not
+userSchema.statics.isUserExistsByCustomId = async function (id: string) {
+  return await User.findOne({ id }).select('+password');
+};
+
+// Check the password is matched or not
+userSchema.statics.isPasswordMatched = async function (
+  plainTextPassword: string,
+  hashedPassword: string,
+) {
+  return await bcrypt.compare(plainTextPassword, hashedPassword);
+};
+
+
+export const User = model<TUser,UserModelType>('User', userSchema);
