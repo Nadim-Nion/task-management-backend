739d8a0250379eac3d8a570437f7966804fe5008
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2025-07-30 22:51:55 +0600

    globalErrorHandler added

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import router from './app/routes';
 import notFound from './app/middlewares/notFound';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
 const app: Application = express();
 
 // Parser / Middleware
@@ -13,6 +14,9 @@ app.use('/api/v1', router);
 
 // Not found route
 app.use(notFound);
+
+// Global Error Handler
+app.use(globalErrorHandler);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello Task Management App! 😊');
