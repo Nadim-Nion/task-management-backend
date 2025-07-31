fa6ddde60541258844960da3616506095b9a02de
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2025-07-31 22:21:03 +0600

    unhandled rejection and uncaught exception handled properly

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,38 @@
 import app from './app';
 import mongoose from 'mongoose';
 import config from './app/config';
+import { Server } from 'http';
+
+let server: Server;
 
 async function main() {
-  try{
+  try {
     await mongoose.connect(config.database_url as string);
 
-  app.listen(config.port, () => {
-    console.log(`Task Management App is listening on port ${config.port} 😊`);
-  });
-  }
-  catch(error){
+    server = app.listen(config.port, () => {
+      console.log(`Task Management App is listening on port ${config.port} 😊`);
+    });
+  } catch (error) {
     console.log(error);
   }
 }
 
 main();
+
+process.on('unhandledRejection', () => {
+  console.log('Unhandled Rejection is detected. Server is Shutting down... 😈');
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+process.on('uncaughtException', () => {
+  console.log('Uncaught Exception is detected. Server is Shutting down... 😈');
+
+  process.exit(1);
+});
